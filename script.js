c30da550d977b088fbfd9ed70a1bec12bd903284
Shreyas Chakraborty <shreyascrny1234@gmail.com>
2024-08-07 20:42:35 +0530

    m

---
+++ script.js
@@ -0,0 +1,10 @@
+function showSidebar()
+{
+    let showSidebar=document.querySelector('.sidebar');
+    showSidebar.style.display='flex';
+}
+function hideSidebar()
+{
+    let hideSidebar=document.querySelector('.sidebar');
+    hideSidebar.style.display='none'
+}
