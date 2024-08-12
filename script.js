2ab26b61ebd733bed074fe22ffae905396db319a
Shreyas Chakraborty <shreyascrny1234@gmail.com>
2024-08-12 19:03:01 +0530

    Added new elements

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ function hideSidebar()
     let hideSidebar=document.querySelector('.sidebar');
     hideSidebar.style.display='none'
 }
+let grid=document.querySelector('#backgroundSpan')
+for(let i=1;i<365;i++)
+{
+    let x=document.createElement("span");
+    grid.appendChild(x);
+}
--- signinstyle/signup.js
+++ signinstyle/signup.js
@@ -1,5 +1,5 @@
 const section1=document.getElementById("section1")
-for(let i=2;i<=365;i++)
+for(let i=1;i<=365;i++)
 {
     let x =document.createElement("span");
     section1.appendChild(x);
