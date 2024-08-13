56b7c143d0e2fdf1a72a07e6b10554e7e776d3c3
Shreyas Chakraborty <shreyascrny1234@gmail.com>
2024-08-14 03:18:30 +0530

    Update

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-function showSidebar()
+//To hide and show sidebar on click
+
+let letsTalk=document.querySelector('#LetsTalk');
+function showSidebar()    //To show sidebar .Function called onclick
 {
     let showSidebar=document.querySelector('.sidebar');
+    letsTalk.style.display='none'
     showSidebar.style.display='flex';
 }
-function hideSidebar()
+function hideSidebar()   //To hide sidebar .Function called when tapped outide or clicked on button
 {
     let hideSidebar=document.querySelector('.sidebar');
-    hideSidebar.style.display='none'
+    hideSidebar.style.display='none';
+    letsTalk.style.display='flex';
 }
+
+//Creating multiple grids for signin page
 let grid=document.querySelector('#backgroundSpan')
-for(let i=1;i<365;i++)
+for(let i=1;i<665;i++)
 {
     let x=document.createElement("span");
     grid.appendChild(x);
 }
-let
